4b87543a6e0d2e7eb2a3772dc7fa957bb7134fe6
Julien Aziz <julien.aziz.extern@k-tc.de>
2023-05-22 12:53:35 +0200

    Added Acquisition Count to acc_ober_budget plotting

--- skactiveml/stream/clustering/test/mining/acc_over_budget.py
+++ skactiveml/stream/clustering/test/mining/acc_over_budget.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 from skactiveml.stream.clustering.test.ExperimentLogger.clu_stream_performance_logger import ACCURACY, BUDGET, \
     CLASSIFIER, BANDWIDTH, REP
 from skactiveml.stream.clustering.util import save_image, run_async
@@ -17,7 +19,17 @@ if __name__ == '__main__':
     sb.set_theme()
 
     f = sb.relplot(data=df_budget, x=BUDGET, y=ACCURACY, kind="line", hue=CLASSIFIER)
+    plt.title('Accuracy')
+
+    # Plotting label aquisition counts
+    label_acquisition = df.groupby([BANDWIDTH, CLASSIFIER, REP, BUDGET])[Y].apply(lambda x: (pd.notna(x)).sum())
+    df_label_acquisition = pd.DataFrame(label_acquisition.reset_index())
+    plt.title('Acquisition Count')
+    g = sb.relplot(
+        data=df_label_acquisition, x=BUDGET, y=Y, kind="line", hue=CLASSIFIER
+    )
 
     image_filepath = os.path.join(this_dir, "..", target_directory, 'output.pdf')
 
     save_image(image_filepath)
+
