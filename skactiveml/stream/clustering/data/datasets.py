172708ad43e55008b6afcd7cf00a26a8f9b6fb76
Julien Aziz <@julien.aziz>
2023-05-27 15:44:55 +0200

    New Artifical Datasets

--- skactiveml/stream/clustering/data/datasets.py
+++ skactiveml/stream/clustering/data/datasets.py
@@ -17,8 +17,11 @@ IRIS = {'name': 'Abalone_binary', 'type': 'openml', 'id': 61, 'toy': True}
 HYPERPLANE = {'name': 'Hyperplane', 'type': 'hyperplane'}
 
 # Local csv sets
+# Real World
 ELECTRICITY = {'name': 'Electricity', 'type': 'local', 'path': os.path.join('real-world', 'elec.csv')}
 
+# Artificial
+CHESSBOARD = {'name': 'ChessBoard', 'type': 'local', 'path': os.path.join('artificial', 'transientChessboard.csv')}
 
 def generate_data(dataset, init_train_length, shuffle, random_state, n_features=0, stream_length=4000, mag_change=0,
                   noise=0.1):
---
+++ skactiveml/stream/clustering/test/mining/acc_over_time_per_budget.py
@@ -0,0 +1,39 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from skactiveml.stream.clustering.test.ExperimentLogger.clu_stream_performance_logger import ACCURACY, BUDGET, \
+    CLASSIFIER, BANDWIDTH, REP, LABEL, CLU_TIMEWINDOW, TIMESTEP
+from skactiveml.stream.clustering.util import save_image, run_async
+
+import pandas as pd
+import seaborn as sb
+import os
+
+
+if __name__ == '__main__':
+    # Window for smoothening the accuracy
+    smoothing_window = 300
+
+    this_dir = os.path.split(__file__)[0]
+    target_directory = 'target'
+    csv_filepath = os.path.join(this_dir, "..", target_directory, 'output.csv')
+    df = pd.read_csv(csv_filepath)
+
+    sb.set_theme()
+
+    df[ACCURACY] = df[ACCURACY].rolling(window=smoothing_window).mean()
+    f = sb.relplot(data=df, x=TIMESTEP, y=ACCURACY, kind="line", hue=CLASSIFIER, col=BUDGET, col_wrap=3, errorbar=None)
+
+    # Plotting label aquisition counts (Probably not needed anymore)
+    '''label_acquisition = df.groupby([BANDWIDTH, CLASSIFIER, REP, BUDGET])[LABEL].apply(lambda x: (pd.notna(x)).sum())
+    df_label_acquisition = pd.DataFrame(label_acquisition.reset_index())
+
+    g = sb.relplot(
+        data=df_label_acquisition, x=BUDGET, y=LABEL, kind="line", hue=CLASSIFIER
+    )
+    g.set(title='Label Acquisition Count')'''
+
+    image_filepath = os.path.join(this_dir, "..", target_directory, 'output_accuracy_over_time.pdf')
+
+    save_image(image_filepath)
+
