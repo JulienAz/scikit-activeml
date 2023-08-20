ea10d59ce66fd4cc7067ff585e8b3e63bb826d34
Julien Aziz <@julien.aziz>
2023-08-20 21:32:26 +0200

    Added Poker hand set

--- skactiveml/stream/clustering/data/datasets.py
+++ skactiveml/stream/clustering/data/datasets.py
@@ -23,6 +23,7 @@ RBF_GENERATOR = {'name': 'RbfGenerator', 'type': 'rbf', 'length': 100000}
 AIRLINES = {'name': 'Airlines', 'length': 539383, 'type': 'local', 'path': os.path.join('real-world', 'airlines2.csv')}
 ELECTRICITY = {'name': 'Electricity', 'length': 45312, 'type': 'local', 'path': os.path.join('real-world', 'elec.csv')}
 COVERTYPE = {'name': 'Covertype', 'length': 581012, 'type': 'local', 'path': os.path.join('real-world', 'covertype.csv')}
+POKER_HAND = {'name': 'PokerHand', 'length': 829201, 'type': 'local', 'path': os.path.join('real-world', 'poker.csv')}
 
 # Artificial
 CHESSBOARD = {'name': 'ChessBoard', 'length': 200000, 'type': 'local', 'path': os.path.join('artificial', 'transientChessboard.csv')}
