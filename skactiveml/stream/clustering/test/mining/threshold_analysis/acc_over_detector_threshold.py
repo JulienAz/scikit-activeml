0b816a6091052599f0d309765e12caa37a54c360
Julien Aziz <@julien.aziz>
2023-08-21 13:38:10 +0200

    Adjusted threshold mining set

--- skactiveml/stream/clustering/test/mining/threshold_analysis/acc_over_detector_threshold.py
+++ skactiveml/stream/clustering/test/mining/threshold_analysis/acc_over_detector_threshold.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from skactiveml.stream.clustering.test.ExperimentLogger.clu_stream_performance_logger import ACCURACY, BUDGET, \
-    CLASSIFIER, BANDWIDTH, REP, LABEL, CLU_TIMEWINDOW, TIMESTEP, N_CLUSTER, DETECTOR_THRESHOLD
+    CLASSIFIER, BANDWIDTH, REP, LABEL, CLU_TIMEWINDOW, TIMESTEP, N_CLUSTER, DETECTOR_THRESHOLD, DATASET
 from skactiveml.stream.clustering.util import save_image, run_async
 
 import pandas as pd
@@ -10,26 +10,21 @@ import seaborn as sb
 import os
 
 if __name__ == '__main__':
-    smoothing_window = 300
-
     hue = CLASSIFIER
     col = N_CLUSTER
-    row = BUDGET
+    row = DATASET
 
-
-    file_name = 'output_acc_over_threshold_per_' + hue + '_' + col + '_' + row + '.pdf'
+    csv_name = 'threshold_minimal.csv'
+    file_name = 'threshold_analysis.pdf'
 
     this_dir = os.path.split(__file__)[0]
     target_directory = 'target'
-    csv_filepath = os.path.join(this_dir, "..", "..", target_directory, 'accuracy_time_window.csv')
+    csv_filepath = os.path.join(this_dir, "..", "..", target_directory, csv_name)
     df = pd.read_csv(csv_filepath)
-
-    df[CLU_TIMEWINDOW] = df[CLU_TIMEWINDOW].replace(np.inf, "Infinity")
-    df[ACCURACY] = df[ACCURACY].rolling(window=smoothing_window).mean()
 
     sb.set_theme()
 
-    f = sb.relplot(data=df, x=DETECTOR_THRESHOLD, y=ACCURACY, kind="line", hue=hue, col=col, row=row, palette='tab10')
+    f = sb.relplot(data=df, x=DETECTOR_THRESHOLD, y=ACCURACY, kind="line", hue=hue, col=col, palette='tab10', facet_kws={'sharey': False})
 
     image_filepath = os.path.join(this_dir, "..", "..", target_directory, file_name)
 
