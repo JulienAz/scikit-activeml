32d65b280cddca81d8857730fb8dcb8759b4cd22
Julien Aziz <@julien.aziz>
2023-08-21 12:34:50 +0200

    Merging script

--- skactiveml/stream/clustering/test/mining/merge_df.py
+++ skactiveml/stream/clustering/test/mining/merge_df.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from skactiveml.stream.clustering.test.ExperimentLogger.clu_stream_performance_logger import ACCURACY, BUDGET, \
-    CLASSIFIER, BANDWIDTH, REP, LABEL
+    CLASSIFIER, BANDWIDTH, REP, LABEL, DATASET
 from skactiveml.stream.clustering.util import save_image, run_async
 
 import pandas as pd
@@ -15,16 +15,16 @@ if __name__ == '__main__':
     target_directory = 'target'
 
 
-    csv_1_filepath = os.path.join(this_dir, "..", target_directory, 'accuracy_time_window.csv')
+    csv_1_filepath = os.path.join(this_dir, "..", target_directory, 'threshold_elec.csv')
 
-    csv_2_filepath = os.path.join(this_dir, "..", target_directory, 'accuracy_budget_elec_hyper.csv')
+    csv_2_filepath = os.path.join(this_dir, "..", target_directory, 'threshold_hyperplane.csv')
 
     df1 = pd.read_csv(csv_1_filepath)
     df2 = pd.read_csv(csv_2_filepath)
 
     concatenated_df = pd.concat([df1, df2], ignore_index=True)
 
-    target_filepath = os.path.join(this_dir, "..", target_directory, 'acc_over_budget_merged.csv')
+    target_filepath = os.path.join(this_dir, "..", target_directory, 'threshold_merged.csv')
 
     concatenated_df.to_csv(target_filepath, index=False)
 
