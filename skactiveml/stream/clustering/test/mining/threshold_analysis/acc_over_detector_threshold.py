4fa131f90ae5fddd3b2246ed42a3b2e49f154d92
Julien Aziz <@julien.aziz>
2023-08-24 13:26:26 +0200

    Adjusted threshold plotting script

--- skactiveml/stream/clustering/test/mining/threshold_analysis/acc_over_detector_threshold.py
+++ skactiveml/stream/clustering/test/mining/threshold_analysis/acc_over_detector_threshold.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
     image_filepath = os.path.join(this_dir, "..", "..", target_directory, file_name)
 
-    #df = df.loc[df[DATASET] != 'Electricity']
+    df = df.loc[df[DATASET] != 'Electricity']
 
     df = df.groupby([REP, BUDGET, CLASSIFIER, N_CLUSTER, DETECTOR_THRESHOLD, DATASET], as_index=False)[ACCURACY].mean()
     sb.set_theme()
@@ -33,6 +33,7 @@ if __name__ == '__main__':
     sb.set_theme()
 
     order_list = ['CORA-SP', 'CORA-SE', 'CORA-EP', 'CORA-EE']
+    cora_colors = ['darkolivegreen', 'firebrick', 'darkcyan', 'purple']
 
     g = sb.relplot(data=df,
                    x=DETECTOR_THRESHOLD,
@@ -42,18 +43,30 @@ if __name__ == '__main__':
                    col=col,
                    row=row,
                    hue_order=order_list,
-                   palette='tab10',
-                   facet_kws={'sharey': False})
+                   palette=cora_colors,
+                   facet_kws={'sharey': 'row'})
 
     g.set_titles(col_template="{col_name} Cluster", row_template="{row_name}")
 
-    #save_image(image_filepath)
     sb.move_legend(
         g, "lower center",
         bbox_to_anchor=(.5, 1), ncol=4, title=None, frameon=False,
     )
-    g.legend
+
+    for ax in g.axes.flat:
+        ax.set_xlabel(ax.get_xlabel(), fontsize=14)  # Adjust x axis label font size
+        ax.set_ylabel(ax.get_ylabel(), fontsize=15)  # Adjust y axis label font size
+        ax.tick_params(axis='both', labelsize=14)
+        ax.set_title(ax.get_title(), fontsize=14)
+
+    leg = g.legend
+
+    for t in leg.get_texts():
+        t.set_fontsize(13)
+
     # Save the figure as a PDF file
-    g.fig.savefig(image_filepath, bbox_inches='tight')
+    g.tight_layout()
+    g.savefig(image_filepath)
+    #g.fig.savefig(image_filepath, bbox_inches='tight')
 
     plt.close()  # Closes the
